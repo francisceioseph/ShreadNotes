c60d4cadd23d891fc4f158a6a54dad093b317961
Francisco José A. C. Souza <francisceioseph@gmail.com>
2015-02-01 10:13:59 -0300

    Correções na GUI e mapeamento dos elementos da GUI para o controller. Mapemento dos eventos da GUI para o controller também.

--- src/sample/Controllers/Controller.java
+++ src/sample/Controllers/Controller.java
@@ -1,4 +1,33 @@
 package sample.Controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
 public class Controller {
+    @FXML
+    TextField emailAddressTextField;
+
+    @FXML
+    PasswordField passwordField;
+
+    @FXML
+    Button loginButton;
+
+    @FXML
+    Button signUpButton;
+
+    @FXML
+    public void signUp(ActionEvent actionEvent){
+
+    }
+
+    @FXML
+    public void login(ActionEvent actionEvent){
+
+    }
+
+
 }
